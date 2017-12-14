7d6967de4e506346bf85538390c13d1433dd7f0f
zahidhassanshaikot <zahidhassanshaikot@gmail.com>
2017-12-14 13:13:02 +0600

    day-13 practice js

---
+++ Day-13/js/my-script.js
@@ -0,0 +1,77 @@
+$('#img1').click(function () {
+    var img1SrcVal=$(this).attr('src');
+    $('#mainImage').attr('src', img1SrcVal);
+    // $('#mainImage').attr('src','');
+});
+
+$('#img2').click(function () {
+    var img1SrcVal=$(this).attr('src');
+    $('#mainImage').attr('src', img1SrcVal);
+    // $('#mainImage').attr('src','');
+});
+
+$('#img3').click(function () {
+    var img1SrcVal=$(this).attr('src');
+    $('#mainImage').attr('src', img1SrcVal);
+    // $('#mainImage').attr('src','');
+});
+
+$('#img4').click(function () {
+    var img1SrcVal=$(this).attr('src');
+    $('#mainImage').attr('src', img1SrcVal);
+    // $('#mainImage').attr('src','');
+});
+
+
+// function createe() {
+//     var newHeadingElement= document.createElement('input');
+//     var newHeadingElement= document.createElement('input');
+//
+//     var newBtnElement= document.createElement('p');
+//
+//     newHeadingElement.innerHTML="this is heading";
+//     newParagraphElement.innerHTML="this is paragraph";
+//
+//     document.getElementById('div').appendChild(newHeadingElement);
+//     document.getElementById('div').appendChild(newParagraphElement);
+//
+// }
+//
+//     document.getElementById('btn').onclick= function createElement() {
+//         createe();
+//     }
+
+// function createe() {
+//     var newHeadingElement= document.createElement('h1');
+//     var newParagraphElement= document.createElement('p');
+//
+//     var newBtnElement= document.createElement('p');
+//
+//     newHeadingElement.innerHTML="this is heading";
+//     newParagraphElement.innerHTML="this is paragraph";
+//
+//     document.getElementById('div').appendChild(newHeadingElement);
+//     document.getElementById('div').appendChild(newParagraphElement);
+//
+// }
+//
+//     document.getElementById('btn').onclick= function createElement() {
+//         createe();
+//     }
+
+// $('#fName').keyup(function () {
+//    var fNameVal=$('#fName').val();
+//    $('#tblFName').text(fNameVal);
+// });
+//
+// $('#lName').keyup(function () {
+//    var lNameVal=$('#lName').val();
+//    $('#tblLName').text(lNameVal);
+// });
+//
+// $('#lName').blur(function () {
+//    var fNameVal=$('#fName').val();
+//    var lNameVal=$('#lName').val();
+//    $('#tblFullName').text(fNameVal+' '+lNameVal);
+// });
+//
