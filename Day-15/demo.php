0cfea276d4ac4b74eba4a8addf12efc30cdbdee1
zahidhassanshaikot <zahidhassanshaikot@gmail.com>
2017-12-21 12:58:14 +0600

    php

---
+++ Day-15/demo.php
@@ -0,0 +1,48 @@
+<?php
+//echo "<html>";
+//    echo "<head>";
+//    echo "<title>";
+//        echo "HOME";
+//    echo "</title>";
+//    echo "</head>";
+//    echo "<body>";
+//        echo "<h1> this is heading</h1>";
+//        echo "<p> this is paragraph</p>";
+//    echo "</body>";
+//
+//echo "</html>";
+
+?>
+
+<!--<html>-->
+<!--<head>-->
+<!--    <title>--><?php //echo "Home"; ?><!--</title>-->
+<!--</head>-->
+<!--<body>-->
+<!--    <h1>--><?php //echo "this is heading"; ?><!--</h1>-->
+<!--    <p> --><?php //echo "this is paragraph"; ?><!--</p>-->
+<!--    <input type="text" value=" --><?php //echo "this is heading"; ?><!--"/>-->
+<!--</body>-->
+<!--</html>-->
+
+<?php
+//3 standard rules for variable
+//start with $ sign
+//A-Z a-z 0-9,_
+//No number in first
+/*
+$firstName="Zahid";
+$lastName="Shaikat";
+
+echo $firstName.$lastName;
+*/
+
+/*
+$x=10;
+$y=20;
+
+echo $x.=$y;
+*/
+
+
+?>
